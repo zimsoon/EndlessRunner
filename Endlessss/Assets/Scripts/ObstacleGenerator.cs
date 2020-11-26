adba2b2225cd60265162237c7fc827433ef143a8
Kamil <kaamlos@wp.pl>
2020-11-26 16:03:56 +0100

    EndlessRunner

---
+++ Endlessss/Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject obstacleObj;
+    List<GameObject> obstaclesOnMap = new List<GameObject>();
+    Vector3 startSpawnPosition = new Vector3(0f, 0f, 10f);
+    float generateDistance = 15f;
+    private IEnumerator coroutine;
+    void Start()
+    {
+        SpawnObstacle(startSpawnPosition);
+        for (int i = 0; i < 49; i++)
+        {
+            SpawnObstacle(obstaclesOnMap[obstaclesOnMap.Count - 1].transform.position);
+        }
+        // Chciałem bardzo 5 ale niestety za mały poziom umiejętności z programowania :(
+
+        coroutine = WaitToSpawn(1f);
+        StartCoroutine(coroutine);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void SpawnObstacle(Vector3 lastObjPosition)
+    {
+        Vector3 spawnPosition = new Vector3(((int)Random.Range(-1.9f, 1.9f) * 3), lastObjPosition.y, lastObjPosition.z + generateDistance);
+        obstaclesOnMap.Add(Instantiate(obstacleObj, spawnPosition, Quaternion.Euler(0, 0, 0)));
+    }
+    IEnumerator WaitToSpawn(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            SpawnObstacle(obstaclesOnMap[obstaclesOnMap.Count - 1].transform.position);
+            if (obstaclesOnMap.Count > 50)
+            {
+                Destroy(obstaclesOnMap[0]);
+                obstaclesOnMap.Remove(obstaclesOnMap[0]);
+            }
+        }
+    }
+}
---
+++ Endlessss/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 newPosition;
+    float speed = 0.15f;
+    int stepDiscance = 3;
+    float x, z;
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(0, 1.1f, 0);
+    }
+
+    void Update()
+    {
+        z = gameObject.transform.position.z + speed;
+        x = gameObject.transform.position.x;
+
+        if (Input.GetKeyDown(KeyCode.A) && x > -stepDiscance)
+        {
+            x = gameObject.transform.position.x - stepDiscance;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && x < stepDiscance)
+        {
+            x = gameObject.transform.position.x + stepDiscance;
+        }
+
+        newPosition = new Vector3(x, gameObject.transform.position.y, z);
+        gameObject.transform.position = newPosition;
+    }
+}
